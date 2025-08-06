2ea4186ae01065b4350114a0bfd9b78a038c5432
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-08-05 19:03:05 -0500

    Update storeAuth.jsx

--- frontend-vet/src/context/storeAuth.jsx
+++ frontend-vet/src/context/storeAuth.jsx
@@ -1,19 +1,20 @@
-import { create } from "zustand"
-import { persist } from "zustand/middleware"
-
+// storeAuth.js
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 const storeAuth = create(
-    persist(
-    set => ({
-        token: null,
-        rol:null,
-        setToken: (token) => set({ token }),
-        setRol: (rol) => set({ rol }),
-        clearToken: () => set({ token: null})
+  persist(
+    (set) => ({
+      token: null,
+      rol: null,
+      id: null, // <-- nuevo
+      setToken: (token) => set({ token }),
+      setRol: (rol) => set({ rol }),
+      setId: (id) => set({ id }), // <-- nuevo setter
+      clearToken: () => set({ token: null, rol: null, id: null }) // limpiar todo
     }),
     { name: "auth-token" }
-    )
-)
+  )
+);
 
-
-export default storeAuth
+export default storeAuth;
