b5f701d4345e151f4dc1f1da7e0bb1d0e972e5b0
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-08-11 00:30:43 -0500

    Update vite.config.js

--- frontend-vet/vite.config.js
+++ frontend-vet/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,               // Esto es equivalente a --host 0.0.0.0
+    port: process.env.PORT || 4173,
+    allowedHosts: ['frontend-production-480a.up.railway.app'], // tu dominio
+  },
 });
