defaf2e66584d8404566abb17554479d4e441b44
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-08-10 22:31:32 -0500

    Update Create.jsx

--- frontend-vet/src/pages/Create.jsx
+++ frontend-vet/src/pages/Create.jsx
@@ -1,4 +1,4 @@
-import { FormProducto } from '../components/create/FormProducto'
+import { FormProducto } from '../components/create/FormProducto';
 
 const Create = () => {
   return (
@@ -17,3 +17,4 @@ const Create = () => {
 }
 
 export default Create
+
